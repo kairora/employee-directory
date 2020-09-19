9cdb598b59c51ddcc37d5e057b4425d03c4a1419
Brianna Bullock <brianna.bullock16@gmail.com>
2020-09-18 23:02:11 -0500

    scaffolds app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Employee Directory</h1>
+        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
