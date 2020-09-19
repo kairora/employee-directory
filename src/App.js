bdde0dfce4f67070be6424fb9f84c72a5418f490
Brianna Bullock <brianna.bullock16@gmail.com>
2020-09-18 23:26:34 -0500

    added table

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Table from "./components/table"
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <h1><i class="fas fa-id-card"></i> Employee Directory</h1>
         <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
       </header>
+      <Table />
     </div>
   );
 }
